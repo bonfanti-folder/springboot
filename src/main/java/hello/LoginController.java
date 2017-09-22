90a1ea47b9c72a2e12786c08753329ec1715f0b5
Mariano Bonfanti <mbonfanti@folderit.net>
2017-09-22 17:13:09 -0300

    Corrección login

--- src/main/java/hello/LoginController.java
+++ src/main/java/hello/LoginController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +13,11 @@ public class LoginController {
 
         @PostMapping("/login")
         public ResponseEntity<?> login(@RequestBody Login input) {
+            HttpHeaders responseHeaders = new HttpHeaders();
+
             if (input.getId().equals(1234) && input.getPassword().equals("1234")) {
-                return ResponseEntity.noContent().header("bearer", "nqghr23iotg8290hi").build();
+                responseHeaders.add("authorization", "bearer:nqghr23iotg8290hi");
+                return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
             } else {
                 throw new LoginFailException();
             }
