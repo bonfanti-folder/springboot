2ef18153751161b7bbea06de307e9bf4ba5cca22
emigioria <emigioria@hotmail.com>
2017-09-29 11:42:25 -0300

    Cambiado ObjectMapper

--- src/main/java/springBoot/Application.java
+++ src/main/java/springBoot/Application.java
@@ -1,7 +1,10 @@
 package springBoot;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println(Application.class.getClassLoader().getResource("").toString());
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT )
+                .enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY );
     }
 }
